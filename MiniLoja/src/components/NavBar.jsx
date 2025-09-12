da3a5ff10b0b7c3d580477f466788561166cea83
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-09-12 19:21:53 -0300

    feat: Header

--- MiniLoja/src/components/NavBar.jsx
+++ MiniLoja/src/components/NavBar.jsx
@@ -8,30 +8,36 @@ export default function NavBar() {
         <NavLink to="/" className={styles.logo}>
           <strong>Shopping</strong>
         </NavLink>
+
+        <nav className={styles.actions}>
+          <div className={styles.links}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? `${styles.NavLink} ${styles.active}` : styles.navLink
+              }
+            >
+              Catalogo
+            </NavLink>
+            <NavLink
+              to="/kanban"
+              className={({ isActive }) =>
+                isActive ? `${styles.NavLink} ${styles.active}` : styles.navLink
+              }
+            >
+              Kanban
+            </NavLink>
+          </div>
+
+          <button type="buton">
+            <span className={styles.themeText}></span>
+          </button>
+
+          <div className={styles.cart}>
+            <span className={styles.cartCount}></span>
+          </div>
+        </nav>
       </div>
-
-      <nav>
-        <div>
-          <NavLink to="/" className={({isActive }) => isActive ? `${styles.NavLink} ${styles.active}` : styles.navLink}>Catalogo</NavLink>
-          <NavLink to="/kanban" className={({isActive }) => isActive ? `${styles.NavLink} ${styles.active}` : styles.navLink}>Kanban</NavLink>
-        </div>
-
-        <button 
-            type="buton" 
-            
-        
-        
-        >
-            <span className={styles.themeText}>
-            
-            </span>
-
-        </button>
-
-        <div className={styles.cart}>
-          <span className={styles.cartCount}></span>
-        </div>
-      </nav>
     </header>
   );
 }
