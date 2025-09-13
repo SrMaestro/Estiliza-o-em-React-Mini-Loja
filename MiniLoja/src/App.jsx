f9a7e5a46a9d58211d1d223378cf69448428b51c
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-09-13 14:22:51 -0300

    feat: Aplicando imagens genericas aos cards de produtos

--- MiniLoja/src/App.jsx
+++ MiniLoja/src/App.jsx
@@ -3,6 +3,8 @@ import NavBar from "./components/NavBar";
 import "./styles/App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import smartphoneImg from "./assets/fone.png";
+
 function App() {
   const products = [
     {
@@ -10,63 +12,42 @@ function App() {
       name: "Smartphone XYZ",
       description: "Celular com câmera de alta resolução e bateria de longa duração",
       price: "1.299,90",
-      image: "/images/smartphone.jpg"
+      image: smartphoneImg
     },
     {
       id: 2,
       name: "Notebook ABC",
       description: "Notebook com 8GB RAM, SSD 256GB e processador Intel i5",
       price: "2.499,90",
-      image: "/images/notebook.jpg"
+      image: smartphoneImg
     },
     {
       id: 3,
       name: "Fone de Ouvido",
       description: "Fone Bluetooth com cancelamento de ruído ativo",
       price: "399,90",
-      image: "/images/headphone.jpg"
+      image: smartphoneImg
     },
     {
       id: 4,
       name: "Smartwatch Pro",
       description: "Relógio inteligente com monitoramento de saúde",
       price: "599,90",
-      image: "/images/smartwatch.jpg"
+      image: smartphoneImg
     },
     {
       id: 5,
       name: "Tablet Ultra",
       description: "Tablet com tela de 10 polegadas e 128GB de armazenamento",
       price: "899,90",
-      image: "/images/tablet.jpg"
+      image: smartphoneImg
     },
     {
       id: 6,
       name: "Câmera Digital",
       description: "Câmera DSLR com lente 18-55mm",
       price: "1.799,90",
-      image: "/images/camera.jpg"
-    },
-    {
-      id: 7,
-      name: "Console Game",
-      description: "Console de última geração com controle wireless",
-      price: "2.199,90",
-      image: "/images/console.jpg"
-    },
-    {
-      id: 8,
-      name: "Mouse Gamer",
-      description: "Mouse óptico com RGB e 6 botões programáveis",
-      price: "199,90",
-      image: "/images/mouse.jpg"
-    },
-    {
-      id: 9,
-      name: "Teclado Mecânico",
-      description: "Teclado mecânico com switches azuis e iluminação RGB",
-      price: "349,90",
-      image: "/images/keyboard.jpg"
+      image: smartphoneImg
     }
   ];
 
